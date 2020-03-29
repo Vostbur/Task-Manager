b7ad8a25dc7428948f5c97276421fb6e07f72728
Alexey Rubtsov <vostbur@gmail.com>
2020-03-29 15:42:13 +0300

    final refactoring. change registartion form

--- app.py
+++ app.py
@@ -34,8 +34,6 @@ logger.add("log.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message
 def index():
     """Route for main page """
     active = None
-    # projects = db.get_projects()
-    # tasks = db.get_tasks()
     projects = db.get_items('project')
     tasks = db.get_items('task')
 
@@ -45,7 +43,6 @@ def index():
                 active = project.project_id
         if not active:
             active = projects[0].project_id
-            # db.update_active_project(projects[0].project_id, 1)
             db.update_item('project', projects[0].project_id, 'active', 1)
     else:
         projects = None
@@ -73,13 +70,10 @@ def add_task():
 
     if not task:
         return redirect('/')
-
     if not project:
-        project = 'Tasks'
+        project = 'Default project'
 
-    # projects = db.get_projects()
     projects = db.get_items('project')
-
     for proj in projects:
         if proj.name == project:
             found = True
@@ -88,20 +82,14 @@ def add_task():
     if not found:
         db.add_project(project)
         logger.info('Пользователь "{}" создал проект "{}".'.format(current_user.username, project))
-        # projects = db.get_projects()
         projects = db.get_items('project')
 
     for proj in projects:
         if proj.name == project:
             project_id = proj.project_id
-            # db.update_active_project(proj.project_id, 1)
-            db.update_item('project', proj.project_id, 'active', 1)
-        else:
-            # db.update_active_project(proj.project_id, 0)
-            db.update_item('project', proj.project_id, 'active', 0)
+        db.update_item('project', proj.project_id, 'active', int(proj.name == project))
 
     status = 1 if bool(int(request.form['status'])) else 0
-
     date = dt.strftime(dt.now(), "%d.%m.%Y")
     db.add_task(project_id, task, date, status)
     logger.info('Пользователь "{}" добавил задачу "{}".'.format(current_user.username, task))
@@ -112,25 +100,10 @@ def add_task():
 @login_required
 def close_task(task_id):
     """Close task """
-    # task = db.get_task_by_id(task_id)
     task = db.get_item('task', task_id)
-
-    if not task:
-        return redirect('/')
-
-    # print(task.status)
-    # if task.status:
-    #     # db.set_task_status(task_id, 0)
-    #     db.update_item('task', task_id, 'status', 0)
-    #     logger.info('Пользователь "{}" отметил задачу "{}" как "выполнено".'.format(current_user.username, task.name))
-    # else:
-    #     # db.set_task_status(task_id, 1)
-    #     db.update_item('task', task_id, 'status', 1)
-    #     logger.info('Пользователь "{}" отметил задачу "{}" как "не выполнено".'.format(current_user.username,
-    #                                                                                    task.name))
-    db.update_item('task', task_id, 'status', abs(task.status-1))
-    logger.info('Пользователь "{}" изменил статус задачи "{}".'.format(current_user.username, task.name))
-
+    if task:
+        db.update_item('task', task_id, 'status', abs(task.status-1))
+        logger.info('Пользователь "{}" изменил статус задачи "{}".'.format(current_user.username, task.name))
     return redirect('/')
 
 
@@ -138,14 +111,10 @@ def close_task(task_id):
 @login_required
 def delete_task(task_id):
     """Delete task """
-    # task = db.get_task_by_id(task_id)
     task = db.get_item('task', task_id)
-
-    if not task:
-        return redirect('/')
-
-    db.delete_item('task', 'id', task_id)
-    logger.info('Пользователь "{}" удалил задачу "{}".'.format(current_user.username, task.name))
+    if task:
+        db.delete_item('task', 'id', task_id)
+        logger.info('Пользователь "{}" удалил задачу "{}".'.format(current_user.username, task.name))
     return redirect('/')
 
 
@@ -153,7 +122,6 @@ def delete_task(task_id):
 @login_required
 def clear_all(project_id):
     """Delete project with tasks """
-    # project = db.get_project_by_id(project_id)
     project = db.get_item('project', project_id)
     db.delete_item('task', 'project', project_id)
     db.delete_item('project', 'id', project_id)
@@ -165,7 +133,6 @@ def clear_all(project_id):
 @login_required
 def remove_all(project_id):
     """Delete all tasks in project """
-    # project = db.get_project_by_id(project_id)
     project = db.get_item('project', project_id)
     db.delete_item('task', 'project', project_id)
     logger.info('Пользователь "{}" удалил все задачи из проекта "{}".'.format(current_user.username, project.name))
@@ -175,17 +142,9 @@ def remove_all(project_id):
 @app.route('/project/<tab>')
 def tab_nav(tab):
     """Change active project """
-    # projects = db.get_projects()
     projects = db.get_items('project')
-
     for project in projects:
-        if project.name == tab:
-            db.update_item('project', project.project_id, 'active', 1)
-            # db.update_active_project(project.project_id, 1)
-        else:
-            # db.update_active_project(project.project_id, 0)
-            db.update_item('project', project.project_id, 'active', 0)
-
+        db.update_item('project', project.project_id, 'active', int(project.name == tab))
     return redirect('/')
 
 
@@ -195,8 +154,7 @@ def login():
         username = request.form['username']
         password = hashlib.sha512(request.form['password'].encode('utf-8')).hexdigest()
         registered_user = users_repository.get_user(username)
-        if registered_user is not None \
-                and registered_user.password == password:
+        if registered_user is not None and registered_user.password == password:
             login_user(registered_user)
             logger.info('Пользователь "{}" начал работу'.format(username))
     return redirect('/')
@@ -214,7 +172,6 @@ def logout():
 def register():
     if request.method == 'GET':
         return render_template('register.html')
-
     username = request.form['username']
     password = hashlib.sha512(request.form['password'].encode('utf-8')).hexdigest()
     new_user = User(username, password, users_repository.next_index())
--- db_utils.py
+++ db_utils.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 
 
 class DataBase:
-    """Class for common database operation """
+    """Class for common database operations """
     db_schema_filename = 'task_manager_schema.sql'
 
     def __init__(self):
@@ -14,37 +14,9 @@ class DataBase:
         self.project = namedtuple('project', 'project_id name active')
         self.task = namedtuple('task', 'task_id project name date status')
 
-    # def get_items(self, item):
-    #     result = []
-    #     for row in self.conn.execute('select * from {}'.format(item)):
-    #         result.append(eval('self.{}(*row)'.format(item)))
-    #     return result
-
     def get_items(self, item):
-        # print(item)
-        # return [exec('self.{}(*row)'.format(item)) for row in self.conn.execute('select * from {}'.format(item))]
         return list(map(lambda row, self=self: eval('self.{}(*row)'.format(item)),
                     self.conn.execute('select * from {}'.format(item))))
-
-    # def get_projects(self):
-    #     result = []
-    #     for row in self.conn.execute('select * from project'):
-    #         result.append(self.project(*row))
-    #     return result
-    #
-    # def get_tasks(self):
-    #     result = []
-    #     for row in self.conn.execute('select * from task'):
-    #         result.append(self.task(*row))
-    #     return result
-
-    # def get_task_by_id(self, task_id):
-    #     row = list(self.conn.execute('select * from task where id = :id', {'id': task_id}))[0]
-    #     return self.task(*row)
-
-    # def get_project_by_id(self, project_id):
-    #     row = list(self.conn.execute('select * from project where id=?', (project_id,)))[0]
-    #     return self.project(*row)
 
     def get_item(self, item, item_id):
         row = next(self.conn.execute('select * from {} where id = :id'.format(item), {'id': item_id}))
@@ -52,18 +24,8 @@ class DataBase:
 
     def delete_item(self, item, field, item_id):
         with self.conn:
-            self.conn.execute('delete from {} where {} = :id'.format(item, field),
-                              {'id': item_id})
+            self.conn.execute('delete from {} where {} = :id'.format(item, field), {'id': item_id})
 
-    # def set_task_status(self, task_id, status):
-    #     with self.conn:
-    #         self.conn.execute('update task set status=? where id=?',
-    #                           (status, task_id))
-    #
-    # def update_active_project(self, project_id, flag):
-    #     with self.conn:
-    #         self.conn.execute('update project set active=? where id=?',
-    #                           (flag, project_id))
     def update_item(self, item, item_id, field, state):
         with self.conn:
             self.conn.execute('update {} set {} = :state where id = :id'.format(item, field),
@@ -71,8 +33,7 @@ class DataBase:
 
     def add_project(self, project):
         with self.conn:
-            self.conn.execute('insert into project (name) \
-                              values (?)', (project,))
+            self.conn.execute('insert into project (name) values (?)', (project,))
 
     def add_task(self, project_id, task, date, status):
         with self.conn:
