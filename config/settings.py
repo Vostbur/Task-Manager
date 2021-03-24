b570b39237af5312e70866d3c198d2fb5dcac3d9
vostbur <vostbur@gmail.com>
2021-03-24 23:45:10 +0300

    change some vars

--- config/settings.py
+++ config/settings.py
@@ -68,14 +68,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# for PostgreSQL	URL = postgres://USER:PASSWORD@HOST:PORT/NAME
-
 DATABASES = {
     'default': env.dj_db_url(
         'DATABASE_URL',
